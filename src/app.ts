2df53f69f296a4b4cb03c1f8994b4ada0aa852f7
JoelRD:01 <eudy.joel.reyes@gmail.com>
2025-06-15 11:55:32 -0400

    Aceptar cabezera

--- src/app.ts
+++ src/app.ts
@@ -13,6 +13,7 @@ const corsOptions = {
   origin: DEPLOY_URL,
 }
 
+app.set('trust proxy', true)
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
